dca55436c78c5bb2afee66e6f9c0082d44536755
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-01-07 19:19:25 +0200

    Installed base project with server logic

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 module.exports = app;
 
@@ -16,10 +19,10 @@ const PORT = process.env.PORT || 4001;
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => console.log(`Server started on port: ${PORT} ...`))
 }
