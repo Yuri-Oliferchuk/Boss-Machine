892c47d76de223383282977b6b5f9a4f0e56800c
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-01-08 17:08:05 +0200

    Added all routers + bonus router. Final version.

--- server/minions.js
+++ server/minions.js
@@ -45,5 +45,46 @@ minionsRouter.delete('/:minionId', (req, res) => {
     deleteFromDatabasebyId('minions', req.params.minionId)
     res.status(204).send();
 });
- 
+
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const work = getAllFromDatabase('work').filter((singleWork) => {
+      return singleWork.minionId === req.params.minionId;
+    });
+    res.send(work);
+  });
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    let newWork = addToDatabase('work', req.body);
+    res.status(201).send(newWork);
+});
+
+minionsRouter.param('workId', (req, res, next, id) => {
+    const work = getFromDatabaseById('work', id);
+    if (work) {
+      req.work = work;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+
+  minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if (req.params.minionId !== req.body.minionId) {
+      res.status(400).send();
+    } else {
+      updatedWork = updateInstanceInDatabase('work', req.body);
+      res.send(updatedWork);
+    }
+  });
+  
+  minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('work', req.params.workId);
+    if (deleted) {
+      res.status(204);
+    } else {
+      res.status(500);
+    }
+    res.send();
+  });
+
 module.exports = minionsRouter;
